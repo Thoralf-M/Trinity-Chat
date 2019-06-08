7e43d89459fe9feed6c479500b86283edbd150fc
Thoralf-M <thoralf.mue@gmail.com>
2019-06-08 17:45:52 +0200

    add package.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const path=require('path');
+const path = require('path');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const converter = require('@iota/converter')
@@ -8,15 +8,15 @@ const txconverter = require('@iota/transaction-converter');
 const units = require('@iota/unit-converter')
 
 app.use(express.static(path.join(__dirname, 'js')));
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on('test', function(socket){
-    socket.on('chat message', function(msg){
-      io.emit('test', msg);
-    });
-    io.emit('test', 'Neuer Client verbunden')
+io.on('test', function (socket) {
+  socket.on('chat message', function (msg) {
+    io.emit('test', msg);
+  });
+  io.emit('test', 'Neuer Client verbunden')
 });
 
 const zmq = require('zeromq')
@@ -36,38 +36,38 @@ sock.on('message', msg => {
   // console.log(msg.toString('utf8')); //raw trytes
   const data = msg.toString().split(' ') // Split to get topic & data
   switch (
-    data[0] // Use index 0 to match topic
+  data[0] // Use index 0 to match topic
   ) {
-    case 'tx_trytes': 
-        if(data[1].slice(2592, 2619)=='TRINITY99999999999999999999'){
-          console.log("tag: "+data[1].slice(2592, 2619))
-          const txobj = txconverter.asTransactionObject(data[1])
-          if (hashes.indexOf(txobj.hash) != -1) {
-            console.log("Already known");
-          } else {
-            var timestamp = Date.now()
-            if (txobj.currentIndex == 0 && txobj.signatureMessageFragment != '9'.repeat(2187) && txobj.attachmentTimestamp>timestamp-100000) {
-              let signature = converter.trytesToAscii(txobj.signatureMessageFragment.slice(0,-1))
-              const pattern = /[^\x19-\xFF]*/g
-              signature = signature.replace(pattern, "");
-              var value=unit(txobj.value)
-              const result = [signature, txobj.hash, parseInt(txobj.attachmentTimestamp.toString().slice(0, 10)), value, txobj.bundle]
-              hashes.push(txobj.hash)
-              io.emit('tx', result)
-              messages.push(result);
-              if (messages.length > maxmessages) {
-                messages.shift();
-              }
+    case 'tx_trytes':
+      if (data[1].slice(2592, 2619) == 'TRINITY99999999999999999999') {
+        console.log("tag: " + data[1].slice(2592, 2619))
+        const txobj = txconverter.asTransactionObject(data[1])
+        if (hashes.indexOf(txobj.hash) != -1) {
+          console.log("Already known");
+        } else {
+          var timestamp = Date.now()
+          if (txobj.currentIndex == 0 && txobj.signatureMessageFragment != '9'.repeat(2187) && txobj.attachmentTimestamp > timestamp - 100000) {
+            let signature = converter.trytesToAscii(txobj.signatureMessageFragment.slice(0, -1))
+            const pattern = /[^\x19-\xFF]*/g
+            signature = signature.replace(pattern, "");
+            var value = unit(txobj.value)
+            const result = [signature, txobj.hash, parseInt(txobj.attachmentTimestamp.toString().slice(0, 10)), value, txobj.bundle]
+            hashes.push(txobj.hash)
+            io.emit('tx', result)
+            messages.push(result);
+            if (messages.length > maxmessages) {
+              messages.shift();
             }
           }
         }
-    break;
-    } 
+      }
+      break;
+  }
 })
 
-function unit(param){
-  var value=param 
-  function round(v, r){value = (Math.round(units.convertUnits(v, 'i', r)* 100)/100)+" "+r}
+function unit(param) {
+  var value = param
+  function round(v, r) { value = (Math.round(units.convertUnits(v, 'i', r) * 100) / 100) + " " + r }
   if (value < 1000) {
     value += ' i'
   } else if (value > 999 && value < 100000) {
@@ -88,14 +88,14 @@ io.on('connection', socket => {
   for (var i = 0, len = messages.length; i < len; i++) {
     io.emit('tx', messages[i])
   }
-  console.log(socket.request.connection.remoteAddress+' connected');
-  socket.on('disconnect', () => { console.log(socket.request.connection.remoteAddress+' disconnected'); });
+  console.log(socket.request.connection.remoteAddress + ' connected');
+  socket.on('disconnect', () => { console.log(socket.request.connection.remoteAddress + ' disconnected'); });
 });
 
-io.on('test', data =>{
-    console.log(data);
+io.on('test', data => {
+  console.log(data);
 })
 
-http.listen(80, '0.0.0.0', ()=>{
-    console.log('listening on *:80');
+http.listen(80, '0.0.0.0', () => {
+  console.log('listening on *:80');
 }) 
